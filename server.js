b49806f163763aeed4e5046267fd64a91ac0d754
Mohamed Lotfi SASSI <mohamedlotfi.sassi@gmail.com>
2016-01-04 18:38:29 +0100

    updated port heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var middleware = require('./middleware.js');
 
@@ -13,6 +13,6 @@ app.get('/about', middleware.requireAuthentification, function (req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(port, function () {
-    console.log('Express server started on port :' + port);
+app.listen(PORT, function () {
+    console.log('Express server started on port :' + PORT);
 });
